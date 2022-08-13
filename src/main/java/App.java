03bb44775b15158c97a738099b04078db91e394f
mvkola <106335750+mvkola@users.noreply.github.com>
2022-08-13 17:48:04 +0300

    This is the solution to task 2.1.2
    Pre-project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,25 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld firstBeanHello =
+                (HelloWorld) applicationContext.getBean("helloWorld");
+        HelloWorld secondBeanHello =
+                (HelloWorld) applicationContext.getBean("helloWorld");
+        System.out.println("For a pair of helloWorld beans the statement is: "
+                + (firstBeanHello == secondBeanHello));
+
+        Cat firstBeanCat =
+                (Cat) applicationContext.getBean("cat");
+        Cat secondBeanCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println("For a pair of cat beans the statement is: "
+                + (firstBeanCat == secondBeanCat));
+
+        System.out.println("helloWorld bean message: " + firstBeanHello.getMessage());
+        System.out.println("cat bean message: " + firstBeanCat.getMessage());
     }
 }
